47509f8caf5722c2bbdae793aa44ef7c2b01119e
elo <146438393+elomendonca@users.noreply.github.com>
2025-04-07 10:34:35 -0300

    aaa

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-12-20T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-16-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2023-02-01T00:00:00");
 
